984589a52fab8533042d60905b994741bd851bc3
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-03-06 23:31:17 +0530

    daily practice

---
+++ class_d3.cpp
@@ -0,0 +1,70 @@
+// MULTI CLASSES
+
+// #include<iostream>
+// using namespace std;
+// class faculty1
+// {
+// public: void show()
+// {
+//   cout<<"Mr.X \n";
+// }
+// };
+// class course 
+// {
+// public: void adrs()
+// {
+//   cout<<"Bhopal \n";
+// }
+// };
+// int main()
+// {
+//   faculty1 f;
+//   f.show();
+//   course c;
+//   c.adrs();
+// }
+
+
+// ANONYMUS OBJECT
+
+// #include<iostream>
+// using namespace std;
+// class {
+// public: void show(){
+//   cout<<"MR.X";
+// }
+// } obj;
+// int main()
+// {
+//   obj.show();
+// }
+
+
+
+
+// FUNCTION OVERLOADING
+
+// #include<iostream>
+// using namespace std;
+// class joy{
+// public: void sum()
+// {
+//     cout<<"Sum Program !! \n ";
+// }
+// void sum(int a,int b)
+// {
+//   cout<<a+b<<"\n";
+// }
+// void sum( int w)
+// {
+//     cout<<w+100;
+// }
+// };
+// int main()
+// {
+//     joy j;
+//     j.sum();
+//     j.sum(3,5);
+//     j.sum(9);
+// }
+
