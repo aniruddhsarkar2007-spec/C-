c399af65859376a309a6a4fbb0967f8577242962
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-02 20:07:55 +0530

    Dsa

--- bubbleosrtprc.cpp
+++ bubbleosrtprc.cpp
@@ -59,49 +59,100 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// int s;
+// cout<<"Enter the size of array:";
+// cin>>s;
+// int arr[s];
+// int temp;
+// int c=0;
+// int flag;
+// cout<<"Enter "<<s<<" Values:\n";
+// for(int i=0;i<s;i++)
+// {
+//     cin>>arr[i];
+// }
+// cout<<"Orignal Array"<<"\n";
+// for(int i=0;i<s;i++)
+// {
+//     cout<<arr[i]<<"\n";
+// }
+// cout<<"Sorted array:\n";
+// for(int i=0;i<s;i++)
+// {flag=0;
+//     for(int j=0;j<s-1-i;j++)
+//     {
+//         if(arr[j]>arr[j+1])
+//         {
+//             temp=arr[i];
+//             arr[i]=arr[j+1];
+//             arr[j+1]=temp;
+//             flag++;
+//         }
+//         c++;
+//     }
+//     if(flag==0)
+//     {
+//         break;
+//     }
+// }
+// for(int i=0;i<s;i++)
+// {
+//     cout<<arr[i]<<"\n";
+// }
+// cout<<"no of iteration:"<<c;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-int s;
-cout<<"Enter the size of array:";
-cin>>s;
-int arr[s];
-int temp;
-int c=0;
-int flag;
-cout<<"Enter "<<s<<" Values:\n";
-for(int i=0;i<s;i++)
-{
-    cin>>arr[i];
-}
-cout<<"Orignal Array"<<"\n";
-for(int i=0;i<s;i++)
-{
-    cout<<arr[i]<<"\n";
-}
-cout<<"Sorted array:\n";
-for(int i=0;i<s;i++)
-{flag=0;
-    for(int j=0;j<s-1-i;j++)
+    int s;
+    cout<<"Enter the size of:";
+    cin>>s;
+    int arr[s];
+    int c=0;
+    int flag;
+    int temp;
+    cout<<"Enter "<<s<<" Values:";
+    for(int i=0;i<s;i++)
     {
-        if(arr[j]>arr[j+1])
-        {
-            temp=arr[i];
-            arr[i]=arr[j+1];
-            arr[j+1]=temp;
-            flag++;
-        }
-        c++;
+        cin>>arr[i];
     }
-    if(flag==0)
+
+    cout<<"Orignal Array:\n";
+    for(int i=0;i<s;i++)
     {
-        break;
+        cout<<arr[i]<<"\n";
     }
-}
-for(int i=0;i<s;i++)
-{
-    cout<<arr[i]<<"\n";
-}
-cout<<"no of iteration:"<<c;
+    cout<<"Sorted Array:\n";
+    for(int i=0;i<s;i++)
+    {
+        flag=0;
+         for(int j=0;j<s-1-i;j++)
+         {
+            if(arr[j]>arr[j+1])
+            {
+             temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+             flag++;
+            }
+            c++;
+         }
+         if(flag==0)
+         {
+            break;
+         }
+    }
+    for(int i=0;i<s;i++)
+    {
+        cout<<arr[i]<<"\n";
+    }
+    cout<<"No of iteration:"<<c;
 }
